c53ba003d1e9b9d490f6e2d2c1f1363f82b6a482
dafonse1 <diogo_rosa92@hotmail.com>
2025-04-11 16:26:41 +0100

    Update registar_desbloqueio.js

--- tmp/registar_desbloqueio.js
+++ tmp/registar_desbloqueio.js
@@ -2,29 +2,12 @@ export const config = {
   runtime: "nodejs"
 };
 
-import fs from "fs";
-import path from "path";
-
 export default function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).end("Método não permitido");
   }
 
-  const data = req.body;
-  const filePath = path.join("/tmp", "desbloqueados.json");
+  console.log("RECEBIDO:", req.body);
 
-  let jsonAtual = {};
-  if (fs.existsSync(filePath)) {
-    const raw = fs.readFileSync(filePath);
-    try {
-      jsonAtual = JSON.parse(raw);
-    } catch (e) {
-      jsonAtual = {};
-    }
-  }
-
-  const novo = { ...jsonAtual, ...data };
-  fs.writeFileSync(filePath, JSON.stringify(novo, null, 2));
-
-  res.status(200).json({ status: "ok" });
+  res.status(200).json({ status: "ok", recebido: req.body });
 }
